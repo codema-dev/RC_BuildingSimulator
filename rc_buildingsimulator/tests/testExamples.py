43eb45953167b3b5663c16e91afdcd8a1e789503
Rowan Molony <rowan.molony@codema.ie>
2021-03-07 21:07:47 +0000

    Mock matplotlib.pyplot.show
    To prevent a plot window from appearing during
    unit testing

--- rc_buildingsimulator/tests/testExamples.py
+++ rc_buildingsimulator/tests/testExamples.py
@@ -1,37 +1,27 @@
-import sys
-import os
 from pathlib import Path
 import unittest
+from unittest.mock import patch
 
 EXAMPLES = Path(__file__).parents[1] / "examples"
 
+
+@patch("matplotlib.pyplot.show")
 class TestBuildingSim(unittest.TestCase):
 
-    def test_annualSimulation(self):
-        runfile = open(EXAMPLES / 'annualSimulation.py')
-        exec(runfile.read())
-        runfile.close()
+    def test_annualSimulation(self, mock_show):
+        import rc_buildingsimulator.examples.annualSimulation
 
-    def test_annualSimulation_importRadiation(self):
-        runfile = open(EXAMPLES / 'annualSimulation_importRadiation.py')
-        exec(runfile.read())
-        runfile.close()
+    def test_annualSimulation_importRadiation(self, mock_show):
+        import rc_buildingsimulator.examples.annualSimulation_importRadiation
 
-    def test_calculateRadiation(self):
-        runfile = open(EXAMPLES / 'calculateRadiation.py')
-        exec(runfile.read())
-        runfile.close()
+    def test_calculateRadiation(self, mock_show):
+        import rc_buildingsimulator.examples.calculateRadiation
 
-    def test_hourSimulation(self):
-        runfile = open(EXAMPLES / 'hourSimulation.py')
-        exec(runfile.read())
-        runfile.close()
+    def test_hourSimulation(self, mock_show):
+        import rc_buildingsimulator.examples.hourSimulation
 
-    def test_sunAngles(self):
-        runfile = open(EXAMPLES / 'sunAngles.py')
-        exec(runfile.read())
-        runfile.close()
-
+    def test_sunAngles(self, mock_show):
+        import rc_buildingsimulator.examples.sunAngles
 
 if __name__ == '__main__':
     unittest.main()
